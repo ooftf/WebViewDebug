925ed739dfe0775759ab9920718a8bb9d4e35b59
ooftf <994749769@qq.com>
2021-05-16 20:37:34 +0800

    sync

--- app/src/main/java/com/ooftf/webview/debug/Const.kt
+++ app/src/main/java/com/ooftf/webview/debug/Const.kt
@@ -11,6 +11,7 @@ object Const {
         "http://dev-web.tojoycloud.org/tojoybossapp/contract.html?userId=77782045&orderId=VIP202101251430006096356&queryType=2&isApp=1&clearNav=1"
     const val URL2 = "https://ooftf.github.io/WebViewDebug/"
     const val URL3 = "https://www.baidu.com"
+    const val URL4 = "https://horeca.cpgroup.unclepi.cool/"
     fun getCurrentUrl(): String {
         return URL2
     }
